a77c2b6c43499483a5c747734de6774caaf49258
Andrew Chen <yongjhih@gmail.com>
2015-02-21 21:51:38 +0800

    Update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-```
+```bash
 ./gradlew -PmainClass=com.github.yongjhih.Main execute
+```
+or with token if 403 forbidden
+```bash
+./gradlew -PmainClass=com.github.yongjhih.Main -Ptoken=abc123 execute # https://github.com/settings/applications#personal-access-tokens
 ```
--- src/main/java/com/github/yongjhih/Main.java
+++ src/main/java/com/github/yongjhih/Main.java
@@ -25,6 +25,10 @@ public class Main {
         String name;
     }
 
+    static class Repo {
+        String full_name;
+    }
+
     interface GitHub {
         @GET("/repos/{owner}/{repo}/contributors")
         //List<Contributor> contributors(
@@ -42,32 +46,55 @@ public class Main {
     }
 
     public static void main(String... args) {
+        String token = null;
+        if (args.length > 1) token = args[1];
+        final String finalToken = token;
+        System.out.println("token: " + finalToken);
         // Create a very simple REST adapter which points the GitHub API endpoint.
         RestAdapter restAdapter = new RestAdapter.Builder()
             .setEndpoint("https://api.github.com")
+            .setRequestInterceptor(request -> {
+                if (finalToken != null && !"".equals(finalToken)) {
+                    request.addHeader("Authorization", "token " + finalToken);
+                }
+            })
             .build();
 
         // Create an instance of our GitHub API interface.
         GitHub github = restAdapter.create(GitHub.class);
 
         // Fetch and print a list of the contributors to this library.
-        /*
+        /* 0.
         List<Contributor> contributors = github.contributors("ReactiveX", "RxJava");
         for (Contributor contributor : contributors) {
             System.out.println(contributor.login + "\t" + contributor.contributions);
         }
         */
 
-        /*
+        /* 1.
         github.contributors("ReactiveX", "RxJava")
             .flatMap(list -> Observable.from(list))
             .forEach(c -> System.out.println(c.login + "\t" + c.contributions));
         */
 
+        /* 2.
         github.contributors("ReactiveX", "RxJava")
             .flatMap(list -> Observable.from(list))
             .flatMap(c -> github.user(c.login))
             .filter(user -> user.name != null)
             .forEach(user -> System.out.println(user.name));
+        */
+
+        github.contributors("ReactiveX", "RxJava")
+            .flatMap(list -> Observable.from(list))
+            .flatMap(c -> github.starred(c.login))
+            .flatMap(list -> Observable.from(list))
+            .filter(r -> !r.full_name.startsWith("ReactiveX"))
+            .groupBy(r -> r.full_name)
+            .flatMap(g -> g.count().map(c -> c + "\t" + g.getKey()))
+            .toSortedList((a, b) -> b.compareTo(a))
+            .flatMap(list -> Observable.from(list))
+            .take(8)
+            .forEach(System.out::println);
     }
 }
