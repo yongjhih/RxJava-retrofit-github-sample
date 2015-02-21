5ba33ee57feb30b5f4396da742acbac6c930db8b
Andrew Chen <yongjhih@gmail.com>
2015-02-21 19:52:54 +0800

    Update

--- src/main/java/com/github/yongjhih/Main.java
+++ src/main/java/com/github/yongjhih/Main.java
@@ -7,6 +7,10 @@ import rx.observables.*;
 import rx.util.*;
 import rx.internal.operators.*;
 
+import java.util.List;
+import retrofit.RestAdapter;
+import retrofit.http.GET;
+import retrofit.http.Path;
 
 /**
  * ref. https://github.com/square/retrofit/blob/master/samples/github-client/src/main/java/com/example/retrofit/GitHubClient.java
@@ -19,10 +23,10 @@ public class Main {
 
     interface GitHub {
         @GET("/repos/{owner}/{repo}/contributors")
-        List<Contributor> contributors(
+        //List<Contributor> contributors(
+        Observable<List<Contributor>> contributors(
             @Path("owner") String owner,
-            @Path("repo") String repo
-            );
+            @Path("repo") String repo);
     }
 
     public static void main(String... args) {
@@ -35,9 +39,14 @@ public class Main {
         GitHub github = restAdapter.create(GitHub.class);
 
         // Fetch and print a list of the contributors to this library.
-        List<Contributor> contributors = github.contributors("square", "retrofit");
+        /*
+        List<Contributor> contributors = github.contributors("ReactiveX", "RxJava");
         for (Contributor contributor : contributors) {
-            System.out.println(contributor.login + " (" + contributor.contributions + ")");
+            System.out.println(contributor.login + "\t" + contributor.contributions);
         }
+        */
+        github.contributors("ReactiveX", "RxJava")
+            .flatMap(list -> Observable.from(list))
+            .forEach(c -> System.out.println(c.login + "\t" + c.contributions));
     }
 }
